dac5e1060b2a940c2a36bbf082335d764566c04d
Matt Weiss <weiss.matt@outlook.com>
2019-03-02 12:14:50 -0700

    ???

---
+++ test/features/user_sees_a_homepage_test.rb
@@ -0,0 +1,15 @@
+require './test/test_helper'
+
+class HomepageTest < CapybaraTestCase
+  def test_user_can_see_the_homepage
+    visit '/'
+    assert page.has_content?("Welcome!")
+    assert_equal 200, page.status_code
+  end
+
+  def test_user_gets_error_page_if_404
+    visit 'blog'
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
+  end
+end
